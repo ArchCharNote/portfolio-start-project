92a3dc881a30cf55b84b88546182b1a250f29550
ArchCharNote <59707178+ArchCharNote@users.noreply.github.com>
2025-11-09 23:42:24 +0400

    init

---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+type MenuPropsType = {
+  items: Array<string>;
+};
+
+export const Menu = (props: MenuPropsType) => {
+  return (
+    <StyledUl>
+      {props.items.map((item, index) => {
+        return (
+          <li key={index}>
+            <a href="#">{item}</a>
+          </li>
+        );
+      })}
+    </StyledUl>
+  );
+};
+
+const StyledUl = styled.ul`
+  display: flex;
+  gap: 60px;
+`;
---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,30 @@
+import { ReactComponent as MoonIcon } from "../../assets/images/moon.svg";
+import { ReactComponent as LogoIcon } from "../../assets/images/logo.svg";
+import { Menu } from "../../components/menu/Menu";
+import styled from "styled-components";
+
+const items = ["Home", "Projects", "About", "Content"];
+
+export const Header = () => {
+  return (
+    <StyledHeader>
+      <LogoIcon />
+      <RightSideHeader>
+        <Menu items={items}></Menu>
+        <MoonIcon />
+      </RightSideHeader>
+    </StyledHeader>
+  );
+};
+
+const StyledHeader = styled.header`
+  background-color: #5c5c5c;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const RightSideHeader = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 60px;
+`;
